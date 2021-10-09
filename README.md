ac8c1a4f1093d5c84b017150fb24a11d3d1c1a0a
Ginny <ginny@Ginnys-MacBook-Pro.local>
2021-10-09 15:35:58 +0300

    chore: basic frontend setups using react-app

---
+++ README.md
@@ -0,0 +1,3 @@
+## Verify students using Github
+
+This application is built to check whether a user is a student of a university in Helsinki, Finland using Github API.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<h2>Verify students using Github</h2>
+		</div>
+	);
+};
+
+export default App;
