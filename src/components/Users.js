0eb247a58355f43aee491769dc4c2a664ca732aa
Ginny-Mac <ginny@powerkernel.com>
2021-10-11 11:07:59 +0300

    feat: get users' emails information and check whether the user is a student of defined universities

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+import { universities } from '../utils/univerisities';
 
 const Users = () => {
+	const history = useHistory();
+	const storedToken = localStorage.getItem('token');
+
+	const [schools, setSchools] = useState([]);
+
+	// if (!storedToken) {
+	// 	history.push('/');
+	// }
+
+	const getUserEmails = async (token) => {
+		const { data } = await axios({
+			method: 'GET',
+			url: 'https://api.github.com/user/emails',
+			headers: {
+				accept: 'application/vnd.github.v3+json',
+				authorization: `token ${token}`,
+			},
+		});
+		return data;
+	};
+
+	const getSchools = (userEmails) => {
+		let schools = [];
+
+		const userEmailsFormat = userEmails.map((userEmail) => {
+			return '@' + userEmail.email.split('@')[1];
+		});
+
+		userEmailsFormat.map((userEmailFormat) => {
+			return universities.filter((university) => {
+				return Object.keys(university).some((uniEmailFormat) => {
+					if (university[uniEmailFormat].includes(userEmailFormat)) {
+						schools.push(university);
+					}
+				});
+			});
+		});
+
+		return schools;
+	};
+
+	useEffect(async () => {
+		const userEmails = await getUserEmails(storedToken);
+		setSchools(getSchools(userEmails));
+	}, []);
 	return (
 		<div>
 			<h2>Users</h2>
+			<ul>
+				{schools.length > 0 &&
+					schools.map((school) => {
+						return <li key={school.name}>{school.name}</li>;
+					})}
+			</ul>
 		</div>
 	);
 };
